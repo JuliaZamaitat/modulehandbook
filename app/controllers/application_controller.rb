adbcf9dd1ca1aaa0bd09706f85a044937213b8ed
JuliaZamaitat <j.zamaitat@gmail.com>
2021-06-18 12:48:00 +0200

    added possibility to access mhb from applications that have an api key.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_or_token
   check_authorization unless: :devise_controller?
+
+
+  private
+  def authenticate_or_token
+    if ENV['RAILS_ENV'] == 'development'
+    if ENV['API_KEY'] == ENV['DEVISE_SECRET_KEY']
+      @current_user =  User.create(email: 'admin@mail.de', password: 'geheim12', password_confirmation: 'geheim12', approved: true, confirmed_at: DateTime.now, role: :admin)
+      return current_user
+    end
+  end
+    authenticate_user!
+end
 end
